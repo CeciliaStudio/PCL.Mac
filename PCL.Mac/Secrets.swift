36734e13dc691936138ebf1922238a81da0acc80
YiZhiMCQiu <yizhimcqiu@icloud.com>
2025-11-24 21:57:31 +0800

    feat: Secrets

--- PCL.Mac/Secrets.swift
+++ PCL.Mac/Secrets.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public let CLIENT_ID: String = "{{CLIENT_ID}}"
 public let THEME_KEY: String = "{{THEME_KEY}}"
+public let API_ROOT: String = "{{API_ROOT}}"
 
 public class Secrets {
     public static func getClientID() -> String {
@@ -17,5 +18,9 @@ public class Secrets {
         }
         
         return ProcessInfo.processInfo.environment["CLIENT_ID"] ?? "" // 本地调试使用
+    }
+    
+    public static func getAPIRoot() -> URL {
+        return URL(string: API_ROOT) ?? URL(string: ProcessInfo.processInfo.environment["PCL_MAC_API_ROOT"] ?? "") ?? URL(string: "https://ceciliastudio.netlify.app/pcl_mac")!
     }
 }
